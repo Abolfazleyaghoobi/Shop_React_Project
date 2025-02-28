fac41ae58344331e49ccd64058dbe01513371375
Abolfazleyaghoobi <134126450+Abolfazleyaghoobi@users.noreply.github.com>
2025-02-28 22:51:32 +0330

    create header component and use to home page

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return ( 
+
+        <>
+        
+        <h3>ali hastam</h3>
+        </>
+     );
+}
+
+export default Header;
--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -1,9 +1,14 @@
+import { Container } from "react-bootstrap";
 import NavBar from "../../components/nav/NavBar";
+import Header from "../../components/header/Header";
 
 function Home() {
     return ( 
        <>
        <NavBar/>
+       <Container>
+        <Header/>
+       </Container>
        </>
 
      );
