0b7bbccb46686194d0d445a3e53232feda5e4b83
Abolfazleyaghoobi <134126450+Abolfazleyaghoobi@users.noreply.github.com>
2025-03-08 22:18:34 +0330

    create component categori and add package axios

--- src/components/cardCategories/Categori.js
+++ src/components/cardCategories/Categori.js
@@ -1,16 +1,19 @@
 import ethnic from "../../assets/img/pic/pic-01.png";
-import "./Categori.css"
-function CardCategori() {
+import "./Categori.css";
+function CardCategori({categori,image}) {
   return (
     <>
-    
-    <div className="cardc position-relative">
-        <h2 className="position-absolute">Ethnic</h2>
-      <div className="con-img">
-        <img className="img-fluid" src={ethnic} alt="" />
+      <div className="color  overflow-hidden">
+        <div className="cardc position-relative">
+          <h2 className="position-absolute model-categori">{categori}</h2>
+
+          <div className="con-img position-relative">
+            <img className="img-fluid" src={image} alt="" />
+          </div>
+          <button className="position-absolute by">{categori} Wear</button>
+        </div>
+
       </div>
-      <button>Ethnic Wear</button>
-    </div>
     </>
   );
 }
--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -1,20 +1,35 @@
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import NavBar from "../../components/nav/NavBar";
 import Header from "../../components/header/Header";
 import CardCategori from "../../components/cardCategories/Categori";
-import "./Home.css"
+import "./Home.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function Home() {
+  let [categories, setCategories] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:2000/categories")
+      .then((e) => setCategories(e.data));
+    console.log(categories);
+  }, []);
   return (
     <>
       <NavBar />
       <Header />
       <Container>
         {/* cardC=>cardCategori */}
-        <section className=" cardC">
-          <h2 >Shop by Categories</h2>
-         <div>
-         <CardCategori />
-         </div>
+
+        <section className="  cardC">
+          <h2>Shop by Categories</h2>
+          <Row>
+            {categories.map((e) => (
+              <Col>
+                <CardCategori {...e} />
+              </Col>
+            ))}
+          </Row>
         </section>
       </Container>
     </>
