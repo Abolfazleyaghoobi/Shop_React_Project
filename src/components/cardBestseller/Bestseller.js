7796ecf78aed47ced5b1062b1777fd7c91279cc6
Abolfazleyaghoobi <134126450+Abolfazleyaghoobi@users.noreply.github.com>
2025-03-16 22:23:43 +0330

    completed box add to card

--- src/components/cardBestseller/Bestseller.js
+++ src/components/cardBestseller/Bestseller.js
@@ -1,10 +1,44 @@
+import { useRef } from "react";
 import image from "../../assets/img/pic/Louis Philippe Sxxport.png";
 
 import "./Bestseller.css"
 function Bestseller({image,name,title,off,price}) {
+  const getBoxAddToCart=useRef(null);
+  const addFavIcon=useRef(null);
+  const showAddToCard=()=>{
+   getBoxAddToCart.current.style="visibility: visible"
+  }
+  const hiddenAddToCard=()=>{
+   getBoxAddToCart.current.style="visibility: hidden"
+  }
+  const addFav=()=>{
+      if(addFavIcon.current.className=="bi bi-star-fill"){
+        addFavIcon.current.className="bi bi-star"
+      }else{
+        addFavIcon.current.className="bi bi-star-fill"
+      }
+      
+   
+  }
+
+
     return ( 
       <>
-      <div className="cardBest mb-4 ">
+      <div onMouseEnter={showAddToCard} onMouseLeave={hiddenAddToCard} className="cardBest mb-4 position-relative">
+      
+        <div ref={getBoxAddToCart} className="addToCard position-absolute">
+         <span className="d-flex ">
+         <p onClick={addFav}>
+          <i ref={addFavIcon} className="bi bi-star"></i>
+          </p>
+          <p>
+          <i className="bi bi-eye"></i>
+          </p>
+         </span>
+          <button className="position-absolute">
+            Add to Card
+          </button>
+        </div>
         <div className="imgBest ">
           <img className="img-fluid" src={image} alt="" />
         </div>
