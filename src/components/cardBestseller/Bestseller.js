17d67508b7906b15b604f323d254894466bb998d
Abolfazleyaghoobi <134126450+Abolfazleyaghoobi@users.noreply.github.com>
2025-03-16 11:13:23 +0330

    add bootstrap-icons package and Completed Bestselle card

--- src/components/cardBestseller/Bestseller.js
+++ src/components/cardBestseller/Bestseller.js
@@ -1,6 +1,33 @@
+import image from "../../assets/img/pic/Louis Philippe Sxxport.png";
+
+import "./Bestseller.css"
 function Bestseller() {
     return ( 
-        <h2>Hello world !</h2>
+      <>
+      <div className="cardBest mb-4 ">
+        <div className="imgBest pt-4">
+          <img className="img-fluid" src={image} alt="" />
+        </div>
+        <div className="infoBest ps-2 pt-4 
+        ">
+            <h5 className="modelName m-0">Roadstar</h5>
+            <p className="title m-0">
+            Printed Cotton T-Shirt
+            </p>
+            <p className="off">
+            <i class="bi bi-currency-dollar"></i>
+            <span>
+              38.00
+            </span>
+            <span className="offer">
+            <i class="bi bi-currency-dollar"></i>
+              40.00
+            </span>
+            </p>
+        </div>
+      </div>
+      
+      </>
 
 
      );
